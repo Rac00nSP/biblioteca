23720b4631d4dd1bac7f8b8bdee0f254866300df
dreck <dreck@localhost.localdomain>
2020-10-13 14:38:19 -0300

    Initial commit

---
+++ app/src/main/java/Clases/precios.java
@@ -0,0 +1,63 @@
+package Clases;
+
+public class precios {
+
+
+    private int farenheith;
+    private int revival;
+    private int elAlquimista;
+    private int elPoder;
+    private int despertar;
+
+    //Constructor
+    public  precios()
+    {
+        farenheith = 5000;
+        revival = 12000;
+        elAlquimista = 45000;
+        despertar = 156000;
+        elPoder = 88000;
+    }
+
+    //Accesadores y Mutadores
+    public int getFarenheith() {
+        return farenheith;
+    }
+
+    public void setFarenheith(int farenheith) {
+        this.farenheith = farenheith;
+    }
+
+    public int getRevival() {
+        return revival;
+    }
+
+    public void setRevival(int revival) {
+        this.revival = revival;
+    }
+
+    public int getElAlquimista() {
+        return elAlquimista;
+    }
+
+    public void setElAlquimista(int elAlquimista) {
+        this.elAlquimista = elAlquimista;
+    }
+
+    public int getElPoder() {
+        return elPoder;
+    }
+
+    public void setElPoder(int elPoder) {
+        this.elPoder = elPoder;
+    }
+
+    public int getDespertar() {
+        return despertar;
+    }
+
+    public void setDespertar(int despertar) {
+        this.despertar = despertar;
+    }
+
+}
