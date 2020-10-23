e0b975abb7b03f11aa9c2aef0833b71871bfa9b4
derekmana <48632269+derekmana@users.noreply.github.com>
2020-10-23 14:36:12 -0300

    Update login.java

--- app/src/main/java/com/example/login/login.java
+++ app/src/main/java/com/example/login/login.java
@@ -1,33 +1,24 @@
 package com.example.login;
-
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
 import java.util.ArrayList;
-
 public class login extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
-
     public void github(View v)
     {
         ArrayList<String> libros = new ArrayList<String>();
-
         libros.add("(Elija un libro)");
         libros.add("Farenheith");
         libros.add("Revival");
         libros.add("El Alquimista");
         libros.add("El Poder");
         libros.add("Despertar");
-
         Intent i = new Intent(this, github.class);
         i.putExtra("listaLibros", libros);
         startActivity(i);
